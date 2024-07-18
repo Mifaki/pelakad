01597462183da088e8abdbac39b73cb1cb4e2d41
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-07-19 04:02:31 +0700

    build: redeploy

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,7 +11,7 @@ import {
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
+ * database instance for multiple projects at once.
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
